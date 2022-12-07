e712fbca36f5e7c4aab23bce7dc7283f345535a7
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-12-07 12:35:12 +0300

    Lib update

--- gulp/tasks/copy-to-dist-fbuildscripts.js
+++ gulp/tasks/copy-to-dist-fbuildscripts.js
@@ -5,11 +5,6 @@ gulp.task(
     function () {
         // console.log("START! copy-to-dist.js");
 
-        return gulp.src(
-            ["./package.json", "./gulp/*/*", "./gulpfile.js", "./npm-link.sh"]
-        )
-            .pipe(
-                gulp.dest("./dist")
-            );
+        return gulp.src(["./package.json", "./gulp/**/*", "./gulpfile.js", "./npm-link.sh"], { base: "." }).pipe(gulp.dest("./dist"))
     }
 );
