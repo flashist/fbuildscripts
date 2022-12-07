a8bd5ccdda7769461157c02b686349debaa235e7
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-12-07 14:22:23 +0300

    Update

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,15 +4,6 @@ var requireDir = require("require-dir");
 var tasks = requireDir("./");
 
 gulp.task(
-    'build-fbuildscripts',
-    gulp.series(
-        "clean-dist",
-        // "generate-definitions",
-        "copy-to-dist-fbuildscripts"
-    )
-);
-
-gulp.task(
     'build-lib-module',
     gulp.series(
         "clean-dist",
