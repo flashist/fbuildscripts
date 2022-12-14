1049572d65ab4e0df89739be9dcc40ab148c072b
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-12-14 12:24:58 +0300

    Update

--- gulp/tasks/npm-dependencies.js
+++ gulp/tasks/npm-dependencies.js
@@ -7,21 +7,23 @@ var prompts = require("prompts");
 // var suggestByAutocompleteScore = tasks.suggestByAutocompleteScore;
 var suggestByAutocompleteScore = require("./interactive-prompts").suggestByAutocompleteScore
 
+const dependencyLibNames = ["@flashist/fbuildscripts", "@flashist/fcore", "@flashist/flibs", "@flashist/fconsole", "@flashist/appframework"]
+
 gulp.task(
     "npm:dependencies:update",
     async (cb) => {
+
+        const libNamesConvertedToChoices = dependencyLibNames.map(
+            (singleLibName) => {
+                return {title: singleLibName}
+            }
+        );
 
         const libResponse = await prompts({
             type: 'autocomplete',
             name: 'value',
             message: 'Choose the lib to update',
-            choices: [
-                { title: "@flashist/fbuildscripts" },
-                { title: "@flashist/fcore" },
-                { title: "@flashist/flibs" },
-                { title: "@flashist/fconsole" },
-                { title: "@flashist/appframework" },
-            ],
+            choices: libNamesConvertedToChoices,
             suggest: suggestByAutocompleteScore
         });
 
@@ -32,14 +34,41 @@ gulp.task(
             message: 'Choose the version to install'
         });
 
-        const dependencyVersion = `${libResponse.value}@${versionResponse.value}`;
-        console.log("Version to install: ", dependencyVersion);
-        return exec(`npm i ${dependencyVersion} --no-save`,
-            function (err, stdout, stderr) {
-                console.log(stdout);
-                console.log(stderr);
-                cb(err);
-            }
-        );
+        // const dependencyVersion = `${libResponse.value}@${versionResponse.value}`;
+        // console.log("Version to install: ", dependencyVersion);
+        // return exec(`npm i ${dependencyVersion} --no-save`,
+        //     function (err, stdout, stderr) {
+        //         console.log(stdout);
+        //         console.log(stderr);
+        //         cb(err);
+        //     }
+        // );
+        await updateLibToVersion(libResponse.value, versionResponse.value);
+        cb();
     }
 );
+
+gulp.task(
+    "npm:all-dependencies:update-to-latest",
+    async (cb) => {
+        const libNamesCount = dependencyLibNames.length;
+        for (let libNameIndex = 0; libNameIndex < libNamesCount; libNameIndex++) {
+            const singleLibName = dependencyLibNames[libNameIndex];
+            await updateLibToVersion(singleLibName, "version");
+        }
+    }
+);
+
+var updateLibToVersion = (libName, version) => {
+    return new Promise(
+        (resolve) => {
+            const dependencyVersion = `${libName}@${version}`;
+            console.log("Version to install: ", dependencyVersion);
+            exec(`npm i ${dependencyVersion} --no-save`,
+                function (err, stdout, stderr) {
+                    resolve(err);
+                }
+            );
+        }
+    )
+}
